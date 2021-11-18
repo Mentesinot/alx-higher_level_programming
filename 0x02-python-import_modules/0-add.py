cb2435157aba01d4435ade623369f289e5f13c07
Mentesinot <mentesinottegegn@gmail.com>
2021-11-18 00:36:41 -0800

    import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def add(a=1,b=2):
-sum=a+b
-print("{} + {} ={}".format(a,b,sum),end=" ")
+if __name__ == "__main__":
+from add_0.py import add
+a=1
+b=2
+print("{}+{}={}".format(a,b,add(a,b))
