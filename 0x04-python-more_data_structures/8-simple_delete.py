2f700d112e82863ddbab38087700e3e84961d142
Mentesinot <mentesinottegegn@gmail.com>
2021-11-24 11:52:29 -0800

    task done

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    """Replace or add key/value pairs in a dictionary."""
-    del a_dictionary[key]
+    if key in a_dictionary:
+        del a_dictionary[key]
     return (a_dictionary)
