6d72024472f1ade652993ece37e8db052b5f744b
Mentesinot <mentesinottegegn@gmail.com>
2021-12-20 12:17:27 -0800

    task done

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """
-inherits from list
+Class Mylist that is Ineritance form class list
 """
+
 
 class MyList(list):
     """
-    class of Mylist inherite from list
+    Args:
+        * var: auxiliar variable in order to not affect the self value
+        * self: variable that refers to the object itself
     """
-
     def print_sorted(self):
-        """
-        print a list
-        :return:
-        """
-        print(sorted(self))
+        var = []
+        var = self.copy()
+        var.sort()
+        print(var)
