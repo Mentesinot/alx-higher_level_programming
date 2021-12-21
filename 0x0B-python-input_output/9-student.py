55b85d85ff87525f679d095a7a476ab9b3145d5a
Mentesinot <mentesinottegegn@gmail.com>
2021-12-21 13:26:56 -0800

    task done

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""student class module"""
+"""Defines a class Student."""
 
 
 class Student:
-    """a class we shall json"""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """init method"""
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """get dict, names if only string"""
-        if type(attrs) is list and all([type(a) == str for a in attrs]):
-            return {name: value for name, value in self.__dict__.items()
-                    if name in attrs}
-        else:
-            return self.__dict__.copy()
-
-    def reload_from_json(self, json):
-        '''gets dict attr from json.'''
-        for key, value in json.items():
-            self.__dict__[key] = 
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
